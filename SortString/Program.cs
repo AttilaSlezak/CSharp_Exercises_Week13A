05774ca5c678bc0165843cca497faf0918fb7c8e
Slezak Attila <attila.slezak@gmail.com>
2016-04-03 23:08:06 +0100

    SortString project added

---
+++ SortString/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Microsoft .NET Framework 2.0 Application Development Foundation";
+            string[] words = text.Split(' ');
+            Array.Sort(words);
+
+            text = String.Join(" ", words);
+            Console.WriteLine(text);
+            Console.ReadKey();
+        }
+    }
+}
